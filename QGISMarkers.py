dacd1b37350a2645f3c5a876c86ed5abcd040713
Steven Jones <stjones@teslamotors.com>
2016-05-06 09:48:52 -0700

    initial Jump to Feature feature

--- QGISMarkers.py
+++ QGISMarkers.py
@@ -7,7 +7,6 @@ from qgis.core import *
 from qgis.gui import QgsMapToolEmitPoint
 import resources_rc
 
-
 class QGISMarkers:
     LOG_TAG = "QGISMarkers"
 
@@ -16,6 +15,7 @@ class QGISMarkers:
         self.iface = iface
         self.map_click_tool = QgsMapToolEmitPoint(iface.mapCanvas())
         self.layer = None
+        self.actions = {}
 
     @staticmethod
     def log(message):
@@ -23,30 +23,55 @@ class QGISMarkers:
 
     def initGui(self):
         self.log("initGui")
+        self.create_place_marker_action()
+        self.create_jump_to_feature_action()
 
+    def create_place_marker_action(self):
         # Menu actions: Plugins->Markers->[Temporary Markers]
-        self.action = QAction(QIcon(":/plugins/qgismarkers/icons/place.png"), "Temporary Markers",
-                              self.iface.mainWindow())
-        self.action.setObjectName("placeMarkerAction")
-        self.action.setWhatsThis("Places a marker at the clicked location.")
-        self.action.setStatusTip("Places a marker and logs `longitude,latitude` to the Log Panel.")
-        QObject.connect(self.action, SIGNAL("triggered()"), self.activate)
+        action = QAction(QIcon(":/plugins/qgismarkers/place.png"), "Temporary Markers", self.iface.mainWindow())
+        action.setObjectName("placeMarkerAction")
+        action.setWhatsThis("Places a marker at the clicked location.")
+        action.setStatusTip("Places a marker and logs `longitude,latitude` to the Log Panel.")
+        QObject.connect(action, SIGNAL("triggered()"), self.activate_place_marker_action)
 
         # Menu: Plugins->[Markers]
-        self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu("&Markers", self.action)
+        self.iface.addToolBarIcon(action)
+        self.iface.addPluginToMenu("&Markers", action)
 
         # Connect to map clicks
         QObject.connect(self.map_click_tool, SIGNAL("canvasClicked(const QgsPoint &, Qt::MouseButton)"), self.click)
 
+        self.actions['place_marker'] = action
+
+    def activate_place_marker_action(self):
+        self.log("activate_place_marker_action")
+        self.iface.mapCanvas().setMapTool(self.map_click_tool)
+
+    def create_jump_to_feature_action(self):
+        # Menu actions: Plugins->Markers->[Temporary Markers]
+        action = QAction(QIcon(":/plugins/qgismarkers/jump.png"), "Jump to Feature", self.iface.mainWindow())
+        action.setObjectName("jumpToFeatureAction")
+        action.setWhatsThis("Jumps to a feature in a given layer.")
+        action.setStatusTip("Jumps to a feature in a given layer.")
+        QObject.connect(action, SIGNAL("triggered()"), self.activate_jump_to_feature_action)
+
+        # Menu: Plugins->[Markers]
+        self.iface.addToolBarIcon(action)
+        self.iface.addPluginToMenu("&Markers", action)
+
+        self.actions['jump_to_feature'] = action
+
+    def activate_jump_to_feature_action(self):
+        self.log("activate_jump_to_feature_action")
+        JumpToFeatureDialog.jump_to_feature(iface=self.iface, parent=self.iface.mainWindow())
+
     def unload(self):
         self.log("unload")
-        self.iface.removePluginMenu("&Markers", self.action)
-        self.iface.removeToolBarIcon(self.action)
-
-    def activate(self):
-        self.log("activate")
-        self.iface.mapCanvas().setMapTool(self.map_click_tool)
+        if not self.actions or self.actions is None:
+            return
+        for name, action in self.actions.items():
+            self.iface.removePluginMenu("&Markers", action)
+            self.iface.removeToolBarIcon(action)
 
     def create_layer(self):
         if self.layer is not None:
@@ -97,3 +122,109 @@ class QGISMarkers:
         self.layer.triggerRepaint()
 
         self.log(longlat)
+
+
+last_jump_dialog_values = {
+    'layer': None
+}
+
+
+class JumpToFeatureDialog(QDialog):
+    def __init__(self, iface=None, parent=None, last_values=None):
+        super(JumpToFeatureDialog, self).__init__(parent)
+        self.setWindowTitle("Jump to Feature")
+        self.iface = iface
+        self.selected_layer = None
+        self.selected_attribute = None
+
+        layout = QFormLayout()
+
+        # Layers
+        box = QComboBox()
+        self.layer_combo_box = box
+        self._populate_layers()
+        box.currentIndexChanged.connect(self._layer_selected)
+        layout.addRow(box)
+
+        # Attributes
+        box = QComboBox()
+        self.attribute_combo_box = box
+        self._populate_attributes()
+        box.currentIndexChanged.connect(self._attribute_selected)
+        layout.addRow(box)
+
+        # Value
+        edit = QLineEdit()
+        self.attribute_edit = edit
+        layout.addRow(edit)
+
+        # Jump
+        button = QPushButton("Jump to Feature")
+        self.jump_button = button
+        button.clicked.connect(self._jump)
+        layout.addRow(button)
+
+        self._layer_selected(0)  # TODO
+        # self.attribute_selected(0)  # TODO
+
+        self.setLayout(layout)
+
+    def _populate_layers(self):
+        layers = QgsMapLayerRegistry.instance().mapLayers()
+        self.layer_combo_box.clear()
+        for name, layer in layers.iteritems():
+            if hasattr(layer, 'attributeList'):
+                self.layer_combo_box.addItem(layer.name(), name)
+
+    def _populate_attributes(self):
+        self.attribute_combo_box.clear()
+        if self.selected_layer is None:
+            return
+        for i in self.selected_layer.attributeList():
+            name = self.selected_layer.attributeDisplayName(i)
+            self.attribute_combo_box.addItem(name, name)
+
+    def _layer_selected(self, i):
+        data = self.layer_combo_box.itemData(i)
+        layer = QgsMapLayerRegistry.instance().mapLayers()[data]
+        self.selected_layer = layer
+        self._populate_attributes()
+
+    def _attribute_selected(self, i):
+        data = self.attribute_combo_box.itemData(i)
+        self.selected_attribute = data
+
+    def _jump(self):
+        layer = self.selected_layer
+        print("Layer: %s" % str(layer))
+        if layer is None:
+            return
+        attribute = self.selected_attribute
+        print("Attribute: %s" % str(attribute))
+        if attribute is None:
+            return
+        value = self.attribute_edit.text()
+        print("Value: %s" % str(value))
+        if value is None:
+            return
+        request = QgsFeatureRequest(QgsExpression("%s = %s" % (attribute, value)))
+        extent = None
+        for feature in layer.dataProvider().getFeatures(request):
+            print("Found feature %s" % str(feature.id()))
+            if extent is None:
+                extent = feature.geometry().boundingBox()
+            else:
+                extent.combineExtentWith(feature.geometry().boundingBox())
+        if extent is None:
+            return
+        source_crs = layer.crs()
+        canvas_crs = self.iface.mapCanvas().mapRenderer().destinationCrs()
+        extent = QgsCoordinateTransform(source_crs, canvas_crs).transformBoundingBox(extent)
+        self.iface.mapCanvas().setExtent(extent)
+        self.iface.mapCanvas().zoomScale(self.iface.mapCanvas().scale() * 1.1)
+        self.iface.mapCanvas().refresh()
+
+    @staticmethod
+    def jump_to_feature(iface=None, parent=None):
+        dialog = JumpToFeatureDialog(iface=iface, parent=parent)
+        result = dialog.exec_()
